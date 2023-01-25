22b40e909adc80760e63cc75e78f61a488df543b
sunil <logins697@gmail.com>
2023-01-25 11:11:41 +0545

    file updtated

--- database/conn.js
+++ database/conn.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
 
-mongoose.connect('mongodb://127.0.0.1:27017/form')
+// mongoose.connect('mongodb://127.0.0.1:27017/form')
+mongoose.connect('mongodb+srv://anil:anil1921@cluster0.634sltj.mongodb.net/userdetails?retryWrites=true&w=majority')
+
 .then(()=>{
     console.log("Mongodb is connected !");
 }).catch((error)=>{
